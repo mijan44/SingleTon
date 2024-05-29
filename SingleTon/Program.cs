adb14a166caed309e45836a2421df2e69c4984d6
mijanur <mijannao@gmail.com>
2024-05-29 16:22:55 +0800

    Add project files.

---
+++ SingleTon/Program.cs
@@ -0,0 +1,17 @@
+﻿using SingleTon;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SingletonLogger singletonLogger1 = new SingletonLogger();
+        SingletonLogger singletonLogger2 = new SingletonLogger();
+
+        singletonLogger1.Log("hello word");
+        singletonLogger2.Log("hi word");
+
+        Console.WriteLine(Object.ReferenceEquals(singletonLogger1, singletonLogger2));
+        Console.WriteLine(string.Join(", ", singletonLogger1.LogMessages));
+        Console.WriteLine(string.Join(", ", singletonLogger2.LogMessages));
+    }
+}
---
+++ SingleTon/SingletonLogger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleTon
+{
+    public class SingletonLogger
+    {
+        private static SingletonLogger _instance;
+        private static readonly object _lock = new object();
+        public List<string> LogMessages = new List<string>();
+        public SingletonLogger()
+        {
+            LogMessages = new List<string>();
+        }
+        public static SingletonLogger Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SingletonLogger();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        public void Log(string message)
+        {
+            LogMessages.Add(message);
+            Console.WriteLine($"Log entry: {message}");
+        }
+
+    }
+}
